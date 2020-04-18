98a1f34419e98aacbfb8964be921b36e54daf2b6
Your Name <you@example.com>
2020-04-18 18:46:31 +0800

    資料合併

--- sortOut.py
+++ sortOut.py
@@ -10,11 +10,9 @@ for f in range(0, 12):
     for x in df_data['num']:
         for i in range(0, 28):
             data.append(x)
-            # print(data)
+
     for y in df_time['0']:
         time.append(y)
 
 df_all11['num'] = data
 df_all11.to_csv('all3.csv', index=False)
-# for y in df_time['0']:
-    # print(y)
